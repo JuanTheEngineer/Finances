3cd16fd62731536b07ffde4bffcc554cabcf3e8b
JuanTheEngineer <juanrodriguezjesus@gmail.com>
2022-11-19 22:57:16 -0500

    Refactor Bofa and Chase to use FinanceFrame class

--- main.py
+++ main.py
@@ -13,67 +13,6 @@ logging.critical('This is a critical message')
 # IDEA: an IDE that views packages as if the files were like photos when you are using File Explorer with the Extra
 # Large Icons on. This is groundbreaking because it will change the way developers think when they structure their
 # repositories.
-def prep_bofa_or_chase_df(df, start_net_worth=0, date_format='%m/%d/%Y', part=1.0, bank=False):
-    df.index.rename('Date', inplace=True)
-    df = df.filter(['Date', 'Amount'], axis=1)
-
-    # convert transactions to floats
-    if isinstance(df['Amount'][0], str):
-        df['Amount'] = pd.to_numeric(df['Amount'].str.strip().str.replace(",", ""))
-
-    df = df.groupby(['Date']).sum()
-
-    # Use pandas.to_datetime() to change datetime format
-    df.index = pd.to_datetime(df.index, format=date_format)
-
-    df = df.sort_index()
-    if bank:
-        df = flatten_income(df)
-
-    # add starting network to first transaction
-    df['Amount'].iat[0] = df['Amount'].iat[0] + start_net_worth
-
-    # 'part' is too unstable mathematically
-    df['Amount'] = df['Amount']  # * part
-
-    return df
-
-
-def flatten_income(df):
-    income_threshold = 3000
-
-    def flattening_complete_df(df_inbound):
-        dates = pd.date_range(df_inbound.index.min(), df_inbound.index.max())
-        df = pd.DataFrame(index=dates)
-        df.index.name = 'Date'
-        df = df.join(df_inbound['Amount'])
-        return df.fillna(0)
-
-    # fill dates in df and add 0's for the transaction balances
-    temp = flattening_complete_df(df)
-
-    # get sum of all values above threshold for counting as income, then split that amongs all dates to simulate rate
-    # of daily income
-    total_income = temp[temp['Amount'] > income_threshold].sum()
-    daily_income = total_income / temp.size
-
-    # erase real income days by setting to 0
-    temp['Amount'] = temp['Amount'].where(temp['Amount'] <= income_threshold, other=0)
-
-    # spread daily income amongst Amount data
-    temp += daily_income
-
-    logging.info("These values should be equal: " + str(temp['Amount'].sum()) + str(df['Amount'].sum()))
-
-    return temp
-
-
-def calc_cumulative_worth_col(df):
-    temp = df
-    temp2 = temp.iloc[::-1]['Amount']
-    temp3 = temp.cumsum()
-    df['NetWorth'] = temp3.iloc[::-1]
-    return complete_df(df)
 
 
 def plot_chart(filename, title, xlabel, ylabel, file_iterator):
@@ -147,20 +86,23 @@ def mint_prep(df):
     return df
 
 
+'''------------------------------------------------------------------------------------------------------------------'''
+
+
 class FinanceFrame:
     DATE = 'Date'
     AMOUNT = 'Amount'
+    NETWORTH = 'NetWorth'
 
     def __init__(self, transaction_file_name, initial_networth, index_col, is_bank=False):
+        self.INCOME_THRESHOLD = 3000
         self.transaction_file_name = transaction_file_name
         self.initial_networth = initial_networth
         self.is_bank = is_bank
         self.df = pd.read_csv(transaction_file_name, index_col=index_col)
         self.hard_copy_of_df = pd.read_csv(transaction_file_name, index_col=index_col)
 
-        # Generic Prep
-        self.correct_column_names()
-        self.correct_data_format()
+        self.networth_df = None
 
     def correct_column_names(self):
         self.df.index.rename(self.DATE, inplace=True)
@@ -173,6 +115,70 @@ class FinanceFrame:
 
         self.df = self.df.groupby([self.DATE]).sum()
 
+    def correct_data_values(self):
+        def complete_df_range():
+            dates = pd.date_range(self.df.index.min(), self.df.index.max())
+            temp = pd.DataFrame(index=dates)
+            temp.index.name = self.DATE
+            self.df = temp.join(self.df[self.AMOUNT])
+            return self.df.fillna(0)
+
+        '''
+        Take all values above income threshold, sum them, set them all to zero on all respective dates, then disperse sum 
+        equally to those days to smooth income over. In the future, we can target all bank deposits to be more accurate.
+        '''
+
+        def flatten_income():
+            temp = self.df
+
+            # get sum of all values above threshold for counting as income, then split that amongst all dates to
+            # simulate rate of daily income
+            total_income = temp[temp[self.AMOUNT] > self.INCOME_THRESHOLD].sum()
+            daily_income = total_income / temp.size
+
+            # erase real income days by setting to 0
+            temp[self.AMOUNT] = temp[self.AMOUNT].where(temp[self.AMOUNT] <= self.INCOME_THRESHOLD, other=0)
+
+            # spread daily income amongst Amount data
+            temp += daily_income
+
+            logging.info("These values should be equal: " + str(temp['Amount'].sum()) + str(self.df['Amount'].sum()))
+            self.df = temp
+
+        self.df = self.df.sort_index()
+        complete_df_range()
+        if self.is_bank:
+            flatten_income()
+
+        # add starting networth to first transaction
+        self.df[self.AMOUNT].iat[0] = self.df[self.AMOUNT].iat[0] + self.initial_networth
+
+        # [WIP] 'part' is too unstable mathematically
+        self.df[self.AMOUNT] = self.df[self.AMOUNT]  # * part
+
+    '''
+    Note: The proof has been solved to state that when we want to combine to data sets, we get the same result if 
+    we group them as transaction data or networth (cumulative) data. 
+    '''
+    def calc_cumulative_worth_df(self):
+        def complete_dataframe():
+            dates = pd.date_range(self.df.index.min(), self.df.index.max())
+            df = pd.DataFrame(index=dates)
+            self.df = df.join(self.df[self.NETWORTH])
+
+        def cleanse_dataframe():
+            self.df.fillna(method="ffill", inplace=True)
+            self.df.fillna(method="bfill", inplace=True)
+
+        temp = self.df
+        temp = temp.iloc[::-1][self.AMOUNT]
+        temp = temp.cumsum()
+        self.networth_df = self.df.copy()
+        self.networth_df.rename(columns={self.AMOUNT: self.NETWORTH})
+        self.networth_df[self.NETWORTH] = temp.iloc[::-1]
+        complete_dataframe()
+        cleanse_dataframe()
+
 
 class BofAFrame(FinanceFrame):
     DATE_FORMAT = '%m/%d/%Y'
@@ -181,22 +187,43 @@ class BofAFrame(FinanceFrame):
         # Look at generic prep in Parent class to see what prep has already been done
         super().__init__(transaction_file_name, initial_networth, index_col, is_bank)
 
-        # BofA specific prep
-        self.prep_bofa()
+        # Generic Prep
+        self.correct_column_names()
+        self.correct_data_format()
 
-    def prep_bofa(self):
+        # BofA specific prep
+        self.prep_bofa_datetime()
+
+        # Generic prep
+        self.correct_data_values()
+        self.calc_cumulative_worth_df()
+
+    def prep_bofa_datetime(self):
         # Use pandas.to_datetime() to change datetime format
         self.df.index = pd.to_datetime(self.df.index, format=self.DATE_FORMAT)
 
-        self.df = self.df.sort_index()
-        if self.is_bank:
-            self.df = self.flatten_income(self.df)
 
-        # add starting network to first transaction
-        self.df[self.AMOUNT].iat[0] = self.df[self.AMOUNT].iat[0] + self.initial_networth
+class ChaseFrame(FinanceFrame):
+    DATE_FORMAT = '%m/%d/%y'
 
-        # [WIP] 'part' is too unstable mathematically
-        self.df[self.AMOUNT] = self.df[self.AMOUNT]  # * part
+    def __init__(self, transaction_file_name, initial_networth, index_col, is_bank=False):
+        # Look at generic prep in Parent class to see what prep has already been done
+        super().__init__(transaction_file_name, initial_networth, index_col, is_bank)
+
+        # Generic Prep
+        self.correct_column_names()
+        self.correct_data_format()
+
+        # BofA specific prep
+        self.prep_bofa_datetime()
+
+        # Generic prep
+        self.correct_data_values()
+        self.calc_cumulative_worth_df()
+
+    def prep_bofa_datetime(self):
+        # Use pandas.to_datetime() to change datetime format
+        self.df.index = pd.to_datetime(self.df.index, format=self.DATE_FORMAT)
 
 
 def run(file_iterator, isBank):
